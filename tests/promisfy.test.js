d4e03fa13f638619231a0979e4497bced0821343
Akshay Khanna <akshay9911102163@gmail.com>
2023-02-14 06:13:05 +0530

    Implement own promisfy()

--- tests/promisfy.test.js
+++ tests/promisfy.test.js
@@ -1,12 +1,27 @@
 const util = require("util");
 
+const promisify = function (fn) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      fn(...args, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  };
+};
+
 describe("promisfy unit test", () => {
   it("Return a promise that resolves with a result of the original function", async () => {
     function originalFn(callback) {
       callback(null, "success");
     }
 
-    const promisifiedFn = util.promisify(originalFn);
+    // const promisifiedFn = util.promisify(originalFn);
+    const promisifiedFn = promisify(originalFn);
     const result = await promisifiedFn();
 
     expect(result).toEqual("success");
@@ -16,7 +31,8 @@ describe("promisfy unit test", () => {
       callback(new Error("error"));
     }
 
-    const promisifiedFn = util.promisify(originalFn);
+    // const promisifiedFn = util.promisify(originalFn);
+    const promisifiedFn = promisify(originalFn);
     let result, error;
     try {
       result = await promisifiedFn();
